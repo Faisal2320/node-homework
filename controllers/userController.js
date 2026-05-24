b920d673c1c9ca716dd4f5213a8263701518bbdb
Faisal <fsaeed033@gmail.com>
2026-05-23 22:43:56 -0400

    user controller with 01. registering with welcome tasks 02. show users with tasks related to them 03. user analytics 04. prisma schema for task and user 05. creating routes for bulk task creating, user analytics , task search and more. 06. query schema

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,6 @@ const util = require("util");
 const scrypt = util.promisify(crypto.scrypt);
 // const pool = require("../db/pg-pool");
 const prisma = require("../db/prisma");
-const { emit } = require("cluster");
 /*
 
 
@@ -25,7 +24,7 @@ async function comparePassword(inputPassword, storedHash) {
 }
 ///////////////////////////////////
 const register = async (req, res, next) => {
-  console.log("This data was posted", JSON.stringify(req.body));
+  // console.log("This data was posted", JSON.stringify(req.body));
   const { error, value } = userSchema.validate(req.body, { abortEarly: false });
   if (error) {
     console.log("VALIDATION ERROR:", error);
@@ -34,22 +33,60 @@ const register = async (req, res, next) => {
       details: error.details,
     });
   }
-
-  let user = null;
+  value.email = value.email.toLowerCase();
   value.hashedPassword = await hashPassword(value.password);
   delete value.password;
 
   try {
-    user = await prisma.user.create({
-      data: {
-        name: value.name,
-        email: value.email,
-        hashedPassword: value.hashedPassword,
-      },
-      select: { name: true, email: true, id: true },
+    const result = await prisma.$transaction(async (tx) => {
+      const newUser = await tx.user.create({
+        data: {
+          name: value.name,
+          email: value.email,
+          hashedPassword: value.hashedPassword,
+        },
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          createdAt: true,
+        },
+      });
+      const welcomeTaskData = [
+        {
+          title: "Complete your profile",
+          userId: newUser.id,
+          priority: "medium",
+        },
+        { title: "Add your first task", userId: newUser.id, priority: "high" },
+        { title: "Explore the app", userId: newUser.id, priority: "low" },
+      ];
+
+      await tx.task.createMany({ data: welcomeTaskData });
+
+      const welcomeTask = await tx.task.findMany({
+        where: {
+          userId: newUser.id,
+          title: {
+            in: welcomeTaskData.map((t) => t.title),
+          },
+        },
+        select: {
+          id: true,
+          title: true,
+          isCompleted: true,
+          userId: true,
+          priority: true,
+        },
+      });
+      return { newUser, welcomeTask };
     });
-    global.user_id = user.id;
-    res.status(StatusCodes.CREATED).json(user);
+    global.user_id = result.newUser.id;
+    return res.status(201).json({
+      user: result.newUser,
+      welcomeTasks: result.welcomeTask,
+      transactionStatus: "success",
+    });
   } catch (e) {
     if (e.name === "PrismaClientKnownRequestError" && e.code === "P2002") {
       return res.status(StatusCodes.BAD_REQUEST).json({
@@ -82,9 +119,11 @@ const logon = async (req, res) => {
 
   global.user_id = user.id;
   console.log("Logged in user: ", global.user_id);
-  return res
-    .status(StatusCodes.OK)
-    .json({ id: user.id, name: user.name, email: user.email });
+  return res.status(StatusCodes.OK).json({
+    id: user.id,
+    name: user.name,
+    email: user.email,
+  });
 };
 // ============================ Logoff
 const logoff = (req, res) => {
@@ -92,7 +131,42 @@ const logoff = (req, res) => {
   return res.sendStatus(StatusCodes.OK);
 };
 
+const show = async (req, res) => {
+  const userId = parseInt(req.params.id);
+
+  if (isNaN(userId)) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ error: "Invalid user ID" });
+  }
+  const user = await prisma.user.findUnique({
+    where: { id: userId },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      createdAt: true,
+      Task: {
+        where: { isCompleted: false },
+        select: {
+          id: true,
+          title: true,
+          priority: true,
+          createdAt: true,
+        },
+        orderBy: { createdAt: "desc" },
+        take: 5,
+      },
+    },
+  });
+  if (!user) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ message: "user not found" });
+  }
+  res.status(StatusCodes.OK).json(user);
+};
 /*
 
 */
-module.exports = { register, logon, logoff };
+module.exports = { register, logon, logoff, show };
